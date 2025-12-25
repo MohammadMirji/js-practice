79669a30ce9729475ce2187964c12b5cdb8f0ab7
Mohammad Mirji <your-email@example.com>
2025-12-25 22:08:00 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const arr = [ 11, 23, 30, 40, 50 ];
+
+// function numbers ( ) {
+//     first = arr.length;
+//     console.log(first);
+// };
+// numbers();
+
+
+for (
+    let i = 0; 
+    i < arr.length;
+    i++)
+    console.log(arr[i]);
+
+// for (const num of arr) {
+//     if ( num % 2 !== 0) {console.log(num + " is odd")};
+// }
+// for (const num of arr) {
+//     if ( num % 2 === 0) {console.log(num + " is even")};
+// }
+
+// const max = Math.min(...arr)
+// console.log(max)
+
+let sum = 0;
+sum += arr[i];
+console.log(sum)
+
