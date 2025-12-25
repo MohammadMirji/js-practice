5dcdb5dea94714583c8b9cf1dc87201d39916763
Mohammad Mirji <your-email@example.com>
2025-12-25 22:43:56 +0530

    Firt

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const arr = [ 11, 23, 30, 40, 50 ];
 
-// function numbers ( ) {
-//     first = arr.length;
-//     console.log(first);
-// };
-// numbers();
+const str = "Mohd";
 
+// 1. Write a Program to reverse a string in JavaScript.
+arr.str();
 
-for (
-    let i = 0; 
-    i < arr.length;
-    i++)
-    console.log(arr[i]);
-
-// for (const num of arr) {
-//     if ( num % 2 !== 0) {console.log(num + " is odd")};
-// }
-// for (const num of arr) {
-//     if ( num % 2 === 0) {console.log(num + " is even")};
-// }
-
-// const max = Math.min(...arr)
-// console.log(max)
-
-let sum = 0;
-sum += arr[i];
-console.log(sum)
-
+console.log(str);
